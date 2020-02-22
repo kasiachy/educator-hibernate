7377c706cad4397a101aafea3b7f54be20e00ce4
kasiachy <kasia.chrobak7@gmail.com>
2020-02-22 13:37:32 +0100

    new class and object created

---
+++ src/main/java/com/bihuniak/educator/basic/Car.java
@@ -0,0 +1,39 @@
+package com.bihuniak.educator.basic;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String model;
+    private String brand;
+    private int wheels;
+    private double engineCapacity;
+    private boolean secondHand;
+
+
+    public Car(String model, String brand, int wheels, double engineCapacity, boolean secondHand) {
+        this.model = model;
+        this.brand = brand;
+        this.wheels = wheels;
+        this.engineCapacity = engineCapacity;
+        this.secondHand = secondHand;
+    }
+
+
+
+    public Car(int id) {
+        this.id = id;
+    }
+
+    public Car() {
+
+    }
+}
